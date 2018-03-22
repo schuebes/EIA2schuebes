18c246c3ebdce648a9de1077ea2128a5165153a7
sven <sven@svens-mbp.eduroam.hs-furtwangen.de>
2018-03-22 13:58:23 +0100

    Mini Website mit Namenabfrage

---
+++ 201/201begrüßung.ts
@@ -0,0 +1,7 @@
+function nameneingabe() {
+  var eingabe = prompt("Hey, was ist dein Name ?");
+  if (eingabe != null) {
+    document.getElementById("antwort").innerHTML =
+      "Nice " + eingabe + "Cool das du da bist und hab noch nen schönen Tag! ";
+  }
+}
