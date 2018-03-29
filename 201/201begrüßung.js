55b0fd23175c82017c937e01857b0ca294e781e1
sven <sven@svens-mbp.eduroam.hs-furtwangen.de>
2018-03-29 11:48:31 +0200

    Verbesserung TypeScript

--- 201/201begrüßung.js
+++ 201/201begrüßung.js
@@ -2,7 +2,7 @@ function nameneingabe() {
     var eingabe = prompt("Hey, was ist dein Name ?");
     if (eingabe != null) {
         document.getElementById("antwort").innerHTML =
-            "Nice " + eingabe + ".  Cool das du da bist und hab noch nen schönen Tag! ";
+            "Nice " + eingabe + "Cool das du da bist und hab noch nen schönen Tag! ";
     }
 }
 //# sourceMappingURL=201begrüßung.js.map
--- 201/201begrüßung.ts
+++ 201/201begrüßung.ts
@@ -1,5 +1,5 @@
-function nameneingabe() {
-  var eingabe = prompt("Hey, was ist dein Name ?");
+function nameneingabe(): void {
+  var eingabe :string  = prompt("Hey, was ist dein Name ?");
   if (eingabe != null) {
     document.getElementById("antwort").innerHTML =
       "Nice " + eingabe + "Cool das du da bist und hab noch nen schönen Tag! ";
